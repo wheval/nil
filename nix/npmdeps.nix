e55b37c9427e71c0f387a4ab5465df3354bfffc5
Gezort <mrle0@nil.foundation>
2025-02-24 17:59:21 +0700

    bump create-nil-hh-proj version

--- nix/npmdeps.nix
+++ nix/npmdeps.nix
@@ -18,5 +18,5 @@ in
       ../uniswap/package.json
     ];
   };
-  hash = "sha256-v2VgVjDalYJoQ8WxCswAGur0nbL/OLL9OpuA/Np1CQ8=";
+  hash = "sha256-MauIU4VV8nGDVoTnrnjFjBOs8TAGjdw+bOmDroyIWL0=";
 })
