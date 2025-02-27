6d9c6421f14172dfa41f7a39362e5de62d1ce804
Oleg Babin <babinoleg@mail.ru>
2025-02-27 12:33:16 +0400

    [types] make Signature types aliases to hexutil.Bytes
    There are no special methods at least for now. Let's remove
    code duplication then.

--- nil/internal/types/signature.go
+++ nil/internal/types/signature.go
@@ -1,41 +1,14 @@
 package types
 
 import (
-	"encoding/hex"
 	"fmt"
 
 	"github.com/NilFoundation/nil/nil/common/hexutil"
 )
 
-type Signature []byte
+type Signature = hexutil.Bytes
 
-func (s Signature) MarshalText() ([]byte, error) {
-	return hexutil.Bytes(s[:]).MarshalText()
-}
-
-func (s *Signature) UnmarshalText(input []byte) error {
-	var b hexutil.Bytes
-	if err := b.UnmarshalText(input); err != nil {
-		return err
-	}
-
-	*s = make(Signature, len(b))
-	copy(*s, b[:])
-	return nil
-}
-
-func (s Signature) Hex() string {
-	enc := make([]byte, hex.EncodedLen(len(s[:]))+2)
-	copy(enc, "0x")
-	hex.Encode(enc[2:], s[:])
-	return string(enc)
-}
-
-func (s Signature) String() string {
-	return s.Hex()
-}
-
-type BlsSignature []byte
+type BlsSignature = hexutil.Bytes
 
 type BlsAggregateSignature struct {
 	Sig  hexutil.Bytes `json:"sig" yaml:"sig" ssz-max:"64"`
