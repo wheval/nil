79a49908162320bce5cf1eb944cfecd528399565
Oleg Babin <babinoleg@mail.ru>
2025-03-12 07:55:19 +0300

    [tests] speedup regression test suite
    This tests are independent. So we can start nilservice only once
    for whole suite.

--- nil/tests/regression/regression_test.go
+++ nil/tests/regression/regression_test.go
@@ -25,9 +25,7 @@ func (s *SuiteRegression) SetupSuite() {
 	var err error
 	s.testAddress, err = contracts.CalculateAddress(contracts.NameTest, 1, []byte{1})
 	s.Require().NoError(err)
-}
 
-func (s *SuiteRegression) SetupTest() {
 	smartAccountValue, err := types.NewValueFromDecimal("10000000000000000000")
 	s.Require().NoError(err)
 	zeroState := &execution.ZeroStateConfig{
@@ -47,7 +45,7 @@ func (s *SuiteRegression) SetupTest() {
 	tests.WaitShardTick(s.T(), s.Context, s.Client, types.BaseShardId)
 }
 
-func (s *SuiteRegression) TearDownTest() {
+func (s *SuiteRegression) TearDownSuite() {
 	s.Cancel()
 }
 
@@ -95,21 +93,18 @@ func (s *SuiteRegression) TestEmptyError() {
 }
 
 func (s *SuiteRegression) TestProposerOutOfGas() {
-	contractCode, abi := s.LoadContract(common.GetAbsolutePath("../contracts/GasBurner.sol"), "GasBurner")
-	deployPayload := s.PrepareDefaultDeployPayload(abi, contractCode)
-
-	addr, receipt := s.DeployContractViaMainSmartAccount(3, deployPayload, types.Value0)
-	s.Require().True(receipt.OutReceipts[0].Success)
+	abi, err := contracts.GetAbi(contracts.NameTest)
+	s.Require().NoError(err)
 
 	calldata, err := abi.Pack("burnGas")
 	s.Require().NoError(err)
 
 	txHash, err := s.Client.SendTransactionViaSmartAccount(s.T().Context(),
 		types.MainSmartAccountAddress, calldata, types.NewFeePackFromGas(100_000_000_000), types.Value0,
-		[]types.TokenBalance{}, addr, execution.MainPrivateKey)
+		[]types.TokenBalance{}, s.testAddress, execution.MainPrivateKey)
 	s.Require().NoError(err)
 
-	receipt = s.WaitIncludedInMain(txHash)
+	receipt := s.WaitIncludedInMain(txHash)
 	s.Require().True(receipt.Success)
 	s.Require().Equal("Success", receipt.Status)
 	s.Require().Len(receipt.OutReceipts, 1)
