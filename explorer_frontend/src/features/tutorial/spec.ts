1813bbd9b27e0e108569a4df8667d03e7b23e1c3
khannanov-nil <khannanov@nil.foundation>
2025-03-18 14:06:01 +0000

    [explorer] adjusted imports for tutorials to avoid unnecessary awaits
    fixes
    
    fixes
    
    fixes

--- explorer_frontend/src/features/tutorial/spec.ts
+++ explorer_frontend/src/features/tutorial/spec.ts
@@ -1,93 +1,86 @@
 import { TutorialLevel } from "./model";
+import {
+  tutorialFiveContracts,
+  tutorialFiveIcon,
+  tutorialFiveText,
+  tutorialFourContracts,
+  tutorialFourIcon,
+  tutorialFourText,
+  tutorialOneContracts,
+  tutorialOneIcon,
+  tutorialOneText,
+  tutorialThreeContracts,
+  tutorialThreeIcon,
+  tutorialThreeText,
+  tutorialTwoContracts,
+  tutorialTwoIcon,
+  tutorialTwoText,
+} from "./tutorialImports";
 
 async function loadTutorials() {
-  const [tutorialOneText, tutorialOneContracts, tutorialOneIcon] = await Promise.all([
-    import("./assets/tutorialOne/tutorialOneText.md?raw"),
-    import("./assets/tutorialOne/tutorialOneContracts.sol?raw"),
-    import("./assets/tutorialOne/tutorialOneIcon.svg"),
-  ]);
+  try {
+    const tutorials = [
+      {
+        stage: 1,
+        text: tutorialOneText,
+        contracts: tutorialOneContracts,
+        icon: tutorialOneIcon,
+        completionTime: "5 minutes",
+        level: TutorialLevel.Easy,
+        title: "Async calls and default tokens",
+        description: "Send an async call between shards.",
+        urlSlug: "async-call",
+      },
+      {
+        stage: 2,
+        text: tutorialTwoText,
+        contracts: tutorialTwoContracts,
+        icon: tutorialTwoIcon,
+        completionTime: "8 minutes",
+        level: TutorialLevel.Easy,
+        title: "Working with custom tokens",
+        description: "Mint and send custom tokens across shards.",
+        urlSlug: "custom-tokens",
+      },
+      {
+        stage: 3,
+        text: tutorialFourText,
+        contracts: tutorialFourContracts,
+        icon: tutorialFourIcon,
+        completionTime: "8 minutes",
+        level: TutorialLevel.Medium,
+        title: "Async deploy",
+        description: "Deploy contracts asynchronously.",
+        urlSlug: "async-deploy",
+      },
+      {
+        stage: 4,
+        text: tutorialThreeText,
+        contracts: tutorialThreeContracts,
+        icon: tutorialThreeIcon,
+        completionTime: "7 minutes",
+        level: TutorialLevel.Medium,
+        title: "Request/response pattern",
+        description: "Manage complex async calls and responses to them.",
+        urlSlug: "request-response",
+      },
+      {
+        stage: 5,
+        text: tutorialFiveText,
+        contracts: tutorialFiveContracts,
+        icon: tutorialFiveIcon,
+        completionTime: "8 minutes",
+        level: TutorialLevel.Medium,
+        title: "NFTs and cross-shard transfers",
+        description: "Send an NFT across shards.",
+        urlSlug: "send-nft",
+      },
+    ];
 
-  const [tutorialTwoText, tutorialTwoContracts, tutorialTwoIcon] = await Promise.all([
-    import("./assets/tutorialTwo/tutorialTwoText.md?raw"),
-    import("./assets/tutorialTwo/tutorialTwoContracts.sol?raw"),
-    import("./assets/tutorialTwo/tutorialTwoIcon.svg"),
-  ]);
-
-  const [tutorialThreeText, tutorialThreeContracts, tutorialThreeIcon] = await Promise.all([
-    import("./assets/tutorialThree/tutorialThreeText.md?raw"),
-    import("./assets/tutorialThree/tutorialThreeContracts.sol?raw"),
-    import("./assets/tutorialThree/tutorialThreeIcon.svg"),
-  ]);
-
-  const [tutorialFourText, tutorialFourContracts, tutorialFourIcon] = await Promise.all([
-    import("./assets/tutorialFour/tutorialFourText.md?raw"),
-    import("./assets/tutorialFour/tutorialFourContracts.sol?raw"),
-    import("./assets/tutorialFour/tutorialFourIcon.svg"),
-  ]);
-
-  const [tutorialFiveText, tutorialFiveContracts, tutorialFiveIcon] = await Promise.all([
-    import("./assets/tutorialFive/tutorialFiveText.md?raw"),
-    import("./assets/tutorialFive/tutorialFiveContracts.sol?raw"),
-    import("./assets/tutorialFive/tutorialFiveIcon.svg"),
-  ]);
-  const tutorials = [
-    {
-      stage: 1,
-      text: tutorialOneText.default,
-      contracts: tutorialOneContracts.default,
-      icon: tutorialOneIcon.default,
-      completionTime: "5 minutes",
-      level: TutorialLevel.Easy,
-      title: "Async calls and default tokens",
-      description: "Send an async call between shards.",
-      urlSlug: "async-call",
-    },
-    {
-      stage: 2,
-      text: tutorialTwoText.default,
-      contracts: tutorialTwoContracts.default,
-      icon: tutorialTwoIcon.default,
-      completionTime: "8 minutes",
-      level: TutorialLevel.Easy,
-      title: "Working with custom tokens",
-      description: "Mint and send custom tokens across shards.",
-      urlSlug: "custom-tokens",
-    },
-    {
-      stage: 3,
-      text: tutorialFourText.default,
-      contracts: tutorialFourContracts.default,
-      icon: tutorialFourIcon.default,
-      completionTime: "8 minutes",
-      level: TutorialLevel.Medium,
-      title: "Async deploy",
-      description: "Deploy contracts asynchronously.",
-      urlSlug: "async-deploy",
-    },
-    {
-      stage: 4,
-      text: tutorialThreeText.default,
-      contracts: tutorialThreeContracts.default,
-      icon: tutorialThreeIcon.default,
-      completionTime: "7 minutes",
-      level: TutorialLevel.Medium,
-      title: "Request/response pattern",
-      description: "Manage complex async calls and responses to them.",
-      urlSlug: "request-response",
-    },
-    {
-      stage: 5,
-      text: tutorialFiveText.default,
-      contracts: tutorialFiveContracts.default,
-      icon: tutorialFiveIcon.default,
-      completionTime: "8 minutes",
-      level: TutorialLevel.Medium,
-      title: "NFTs and cross-shard transfers",
-      description: "Send an NFT across shards.",
-      urlSlug: "send-nft",
-    },
-  ];
-  return tutorials;
+    return tutorials;
+  } catch (e) {
+    console.log(e);
+  }
 }
 
 export default loadTutorials;
