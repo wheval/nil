678f2c8722ffe82e93e9eaca69659f100cb29ea2
idea404 <dennis.oliver.huisman@gmail.com>
2025-04-07 13:09:52 +0200

    bring back the raw imports of nil and prepop sources

--- explorer_frontend/src/features/shared/utils/solidityCompiler/helper.ts
+++ explorer_frontend/src/features/shared/utils/solidityCompiler/helper.ts
@@ -1,5 +1,10 @@
 import { processImports } from "./processImports";
 
+import FaucetSol from "@nilfoundation/smart-contracts/contracts/Faucet.sol?raw";
+import NilSol from "@nilfoundation/smart-contracts/contracts/Nil.sol?raw";
+import NilTokBaseSol from "@nilfoundation/smart-contracts/contracts/NilTokenBase.sol?raw";
+import SmartAccountSol from "@nilfoundation/smart-contracts/contracts/SmartAccount.sol?raw";
+
 // biome-ignore lint/suspicious/noExplicitAny: <explanation>
 export const createCompileInput = async (
   contractBody: string,
@@ -8,6 +13,20 @@ export const createCompileInput = async (
   const sources: Record<string, { content: string }> = {
     Compiled_Contracts: {
       content: contractBody,
+    },
+    "Faucet.sol": { content: FaucetSol },
+    "@nilfoundation/smart-contracts/contracts/Faucet.sol": {
+      content: FaucetSol,
+    },
+    "Nil.sol": { content: NilSol },
+    "@nilfoundation/smart-contracts/contracts/Nil.sol": { content: NilSol },
+    "NilTokenBase.sol": { content: NilTokBaseSol },
+    "@nilfoundation/smart-contracts/contracts/NilTokenBase.sol": {
+      content: NilTokBaseSol,
+    },
+    "SmartAccount.sol": { content: SmartAccountSol },
+    "@nilfoundation/smart-contracts/contracts/SmartAccount.sol": {
+      content: SmartAccountSol,
     },
   };
 
