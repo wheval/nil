6c913d0d1c5c9cd7c8db5497af82e4e1ce6367ac
Oleg Babin <babinoleg@mail.ru>
2025-04-18 17:55:45 +0400

    [smart-contracts] implement Awaiter contract
    This patch reimplements some async context logic in pure EVM.

--- explorer_frontend/src/features/tutorial/assets/tutorialThree/tutorialThreeSolution.sol
+++ explorer_frontend/src/features/tutorial/assets/tutorialThree/tutorialThreeSolution.sol
@@ -3,26 +3,23 @@
 pragma solidity ^0.8.21;
 
 import "@nilfoundation/smart-contracts/contracts/Nil.sol";
+import "@nilfoundation/smart-contracts/contracts/NilAwaitable.sol";
 
-contract Requester is NilBase {
+contract Requester is NilBase, NilAwaitable {
     using Nil for address;
     uint256 private num1 = 5;
     uint256 private num2 = 10;
     bool private result;
 
     function requestMultiplication(address dst) public payable {
-        bytes memory context = abi.encodeWithSelector(
-            this.checkResult.selector,
-            num1,
-            num2
-        );
+        bytes memory context = abi.encode(num1, num2);
         bytes memory callData = abi.encodeWithSignature(
             "multiply(uint256,uint256)",
             num1,
             num2
         );
 
-        Nil.sendRequest(dst, 0, Nil.ASYNC_REQUEST_MIN_GAS, context, callData);
+        sendRequest(dst, 0, Nil.ASYNC_REQUEST_MIN_GAS, context, callData, checkResult);
     }
 
     function checkResult(
