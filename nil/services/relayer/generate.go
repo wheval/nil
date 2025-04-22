a2e81f749b924a329666460fe7d6a22bda2ab67b
akokoshn <alexeikokoshnikov@nil.foundation>
2025-04-22 16:41:50 +0300

    [rollup-bridge-contracts] Remove go generate on relayer ABI preparation

--- nil/services/relayer/generate.go
+++
@@ -1,8 +0,0 @@
-package relayer
-
-import "embed"
-
-//go:generate bash -c "solc ../../../rollup-bridge-contracts/contracts/bridge/l1/interfaces/IRelayMessage.sol --abi --overwrite -o ../rollup-bridge-contracts-compiled-abi/contracts/bridge/l1/L1BridgeMessenger.sol --allow-paths .,../../../rollup-bridge-contracts/contracts/common/libraries --no-cbor-metadata --metadata-hash none --pretty-json"
-//go:generate bash -c "solc ../../../rollup-bridge-contracts/contracts/bridge/l2/interfaces/IRelayMessage.sol --abi --overwrite -o ../rollup-bridge-contracts-compiled-abi/contracts/bridge/l2/L2BridgeMessenger.sol --allow-paths .,../../../rollup-bridge-contracts/contracts/common/libraries --no-cbor-metadata --metadata-hash none --pretty-json"
-
-var Fs embed.FS
